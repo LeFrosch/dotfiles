3547cc32844ec41237c93a3b9771b9abb5185e96
Daniel Brauner <44034965+LeFrosch@users.noreply.github.com>
2023-10-08 22:04:25 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install zsh-vi-mode plugin
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
