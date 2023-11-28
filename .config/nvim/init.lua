be13b019a079d6ae5f9d2b258c355acba63ca71a
Daniel Brauner <daniel-brauner@outlook.com>
2023-11-28 23:01:09 +0100

    [neovim] added autosave

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,8 +22,11 @@ vim.o.relativenumber = true
 vim.o.scrolloff = 8
 vim.o.hlsearch = true
 vim.o.undofile = true
+vim.o.autowriteall = true
 
 vim.o.completeopt = 'menuone,noselect'
+
+vim.o.mouse = ''
 
 -- mappings
 local mapper = function(mode, key, result, desc)
@@ -50,6 +53,9 @@ mapper('n', '<C-s>', '<cmd>wa<cr>', 'Save all')
 -- buffer mappings
 mapper('n', '<leader>bv', '<cmd>bo vs<cr>', '[B]uffer Split [V]ertical')
 mapper('n', '<leader>bh', '<cmd>bo sp<cr>', '[B]uffer Split [H]orizontal')
+
+-- autosave
+vim.cmd('autocmd! BufLeave * if &modifiable && &buftype ==# "" | :update | endif')
 
 -- plugins
 require('config.lazy')
