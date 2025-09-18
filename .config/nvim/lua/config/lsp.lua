6cc7429eacde262e21f8fd955375ca6c5225f5b1
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-09-18 18:06:51 +0200

    [nvim] updated lsp configs

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -12,60 +12,45 @@ require('neodev').setup()
 
 local telescope = require('telescope.builtin')
 
-local function on_attach(_, bufnr)
-  local function opts(desc)
-    return { desc = 'LSP: ' .. desc, buffer = bufnr, noremap = true }
-  end
-
-  local function search(func)
-    return function()
-      func { initial_mode = 'insert' }
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local function opts(desc)
+      return { desc = 'LSP: ' .. desc, buffer = ev.buf, noremap = true, silent = true }
     end
-  end
 
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts('[R]e[n]ame'))
+    local function search(func)
+      return function()
+        func { initial_mode = 'insert' }
+      end
+    end
 
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts('[C]ode [A]ction'))
-
-  vim.keymap.set('n', 'gd', search(telescope.lsp_definitions), opts('[G]oto [D]efinition'))
-  vim.keymap.set('n', 'gr', search(telescope.lsp_references), opts('[G]oto [R]eferences'))
-  vim.keymap.set('n', 'gI', search(telescope.lsp_implementations), opts('[G]oto [I]mplementations'))
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts('[G]oto [D]eclaration'))
-
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts('Hover Documentation'))
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts('Signature Documentation'))
-end
-
-local function on_attach_dart(_, bufnr)
-  on_attach(nil, bufnr)
-
-  local function opts(desc)
-    return { desc = 'Dart: ' .. desc, buffer = bufnr, noremap = true }
-  end
-
-  local reload_cmd = [[<cmd>wa | silent execute '!kill -s USR1 "$(pgrep -f flutter_tools.snapshot\ run)"'<cr>]]
-  vim.keymap.set('n', '<leader>cr', reload_cmd, opts('Flutter Hot [R]eload'))
-end
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts('[R]e[n]ame'))
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts('[C]ode [A]ction'))
+    vim.keymap.set('n', 'gd', search(telescope.lsp_definitions), opts('[G]oto [D]efinition'))
+    vim.keymap.set('n', 'gr', search(telescope.lsp_references), opts('[G]oto [R]eferences'))
+    vim.keymap.set('n', 'gI', search(telescope.lsp_implementations), opts('[G]oto [I]mplementations'))
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts('[G]oto [D]eclaration'))
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts('Hover Documentation'))
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts('Signature Documentation'))
+  end,
+})
 
 local servers = {
   -- Lua
   lua_ls = {
-    settings = {
-      Lua = {
-        workspace = { checkThirdParty = false },
-        telemetry = { enable = false },
-      },
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
     },
   },
 
   -- Dart
   dartls = {
-    settings = {
-      dart = {
-        lineLength = 100
-      }
-    },
-    on_attach = on_attach_dart,
+    dart = {
+      lineLength = 100
+    }
   },
 
   -- Java
@@ -76,10 +61,8 @@ local servers = {
 
   -- Elixir
   elixirls = {
-    settings = {
-      elixirLS = {
-        dialyzerEnabled = false
-      }
+    elixirLS = {
+      dialyzerEnabled = false
     }
   },
 
@@ -90,7 +73,16 @@ local servers = {
   zls = {},
 
   -- Python
-  pylsp = {},
+  pylsp = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          enabled = true,
+          maxLineLength = 120,
+        },
+      },
+    },
+  },
 
   -- Haskell,
   hls = {},
@@ -100,9 +92,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 for server, settings in pairs(servers) do
-  require('lspconfig')[server].setup {
-    capabilities = capabilities,
-    on_attach = settings.on_attach or on_attach,
-    settings = settings.settings,
-  }
+  vim.lsp.enable(server)
+  vim.lsp.config(server, { capabilities = capabilities, settings = settings })
 end
+
